938e7da6def0001e3035eff7c836aa1b5e55179d
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2025-01-17 20:51:21 +0200

    Connect your phone and emulator at the same time

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+} from "react-native";
 
 export default function App() {
   return (
@@ -8,7 +14,7 @@ export default function App() {
         style={styles.image}
         source={require("./assets/images/bird.jpg")}
       >
-        <Text style={styles.text}>Open up App!</Text>
+        <Text style={styles.text}>Open up App!!!</Text>
         <StatusBar style="light" />
       </ImageBackground>
     </View>
@@ -25,11 +31,42 @@ const styles = StyleSheet.create({
   text: {
     color: "#fff",
     fontSize: 30,
+    paddingTop: 640,
   },
   image: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
   },
 });
+
+// import React from "react";
+// import { View, Text, StyleSheet } from "react-native";
+
+// export default App = () => (
+//   <View style={styles.container}>
+//     <View style={styles.box}>
+//       <Text style={styles.text}>I take all available space!</Text>
+//     </View>
+//   </View>
+// );
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center", // Выравнивание по вертикали
+//     alignItems: "center", // Выравнивание по горизонтали
+//   },
+//   box: {
+//     flex: 1,
+//     backgroundColor: "green", // Цвет фона для выделения
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   text: {
+//     paddingHorizontal: 10,
+//     fontSize: 20,
+//     color: "#fff",
+//   },
+// });
